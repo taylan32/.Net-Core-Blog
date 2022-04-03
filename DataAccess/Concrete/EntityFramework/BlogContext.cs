e273db911ee24f8fee1101a29ceaa25695bcbbd4
Taylan <taylanalparslan@hotmail.com>
2022-04-03 14:02:38 +0300

    Entity Framework added

---
+++ DataAccess/Concrete/EntityFramework/BlogContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class BlogContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;database=Blog;Trusted_Conntection=true");
+        }
+
+        public DbSet<Category> categories { get; set; }
+    }
+}
