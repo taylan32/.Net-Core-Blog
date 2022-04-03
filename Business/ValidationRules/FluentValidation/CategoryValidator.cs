b788ff6c8c9bffef8d0da0cb649c1af70437d111
Taylan <taylanalparslan@hotmail.com>
2022-04-03 14:36:57 +0300

    Fluent Validation added

---
+++ Business/ValidationRules/FluentValidation/CategoryValidator.cs
@@ -0,0 +1,19 @@
+﻿using Business.Constants;
+using Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty().WithMessage(Messages.CategoryDescriptionEmpty);
+        }
+    }
+}
