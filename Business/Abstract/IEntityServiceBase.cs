b4c8b973aa6ac47ca1731df1aee1d87e4916cdaa
Taylan <taylanalparslan@hotmail.com>
2022-04-03 14:22:35 +0300

    Autofac added

---
+++ Business/Abstract/IEntityServiceBase.cs
@@ -0,0 +1,18 @@
+﻿using Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface  IEntityServiceBase<T>
+    {
+        Result Add(T entity);
+        Result Delete(T entity);
+        Result Update(T entity);
+        IDataResult<List<T>> GetAll();
+        IDataResult<T> GetById(int id);
+    }
+}
---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        private ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+        public Result Add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result Delete(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<Category>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<Category> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
